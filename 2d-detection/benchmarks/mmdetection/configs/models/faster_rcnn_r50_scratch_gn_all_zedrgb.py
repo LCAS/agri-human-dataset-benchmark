0789061a5cc9ab32432fc584196b2e373e74144d
Dang Vu <tamquattnb@gmail.com>
2026-03-15 21:32:09 +0000

    add benchmark configurations (from scratch) for Faster R-CNN on various datasets including coco2017, fieldsafepedestrian, fisheye, kitti, and zedrgb

---
+++ 2d-detection/benchmarks/mmdetection/configs/models/faster_rcnn_r50_scratch_gn_all_zedrgb.py
@@ -0,0 +1,70 @@
+_base_ = '../../../../third_party/mmdetection/configs/scratch/faster-rcnn_r50-scratch_fpn_gn-all_6x_coco.py'
+
+dataset_type = 'ZedrgbDataset'
+data_root = '/workspace/data/zedrgb-coco/'
+
+metainfo = dict(classes=('person',))
+
+backend_args = None
+
+model = dict(roi_head=dict(bbox_head=dict(num_classes=1)))
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='annotations/instances_train.json',
+        data_prefix=dict(img='images/train/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='annotations/instances_val.json',
+        data_prefix=dict(img='images/val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/instances_val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
+
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
+
+load_from = None
