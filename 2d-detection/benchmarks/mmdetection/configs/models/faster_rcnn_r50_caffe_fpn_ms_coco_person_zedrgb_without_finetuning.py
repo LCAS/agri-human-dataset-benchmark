c4635de4e0c12988855911f933f04737c5b0258d
Dang Vu <tamquattnb@gmail.com>
2026-03-15 21:02:53 +0000

    define the dataset settings inline in each file to avoid duplicate-base error

--- 2d-detection/benchmarks/mmdetection/configs/models/faster_rcnn_r50_caffe_fpn_ms_coco_person_zedrgb_without_finetuning.py
+++ 2d-detection/benchmarks/mmdetection/configs/models/faster_rcnn_r50_caffe_fpn_ms_coco_person_zedrgb_without_finetuning.py
@@ -1,6 +1,68 @@
-_base_ = [
-    '../../../../third_party/mmdetection/configs/faster_rcnn/faster-rcnn_r50-caffe_fpn_ms-1x_coco-person.py',
-    '../datasets/zedrgb_detection.py',
+_base_ = '../../../../third_party/mmdetection/configs/faster_rcnn/faster-rcnn_r50-caffe_fpn_ms-1x_coco-person.py'
+
+dataset_type = 'ZedrgbDataset'
+data_root = '/workspace/data/zedrgb-coco/'
+
+metainfo = dict(classes=('person',))
+
+backend_args = None
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
 ]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='annotations/instances_train.json',
+        data_prefix=dict(img='images/train/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline,
+        backend_args=backend_args))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='annotations/instances_val.json',
+        data_prefix=dict(img='images/val/'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'annotations/instances_val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=backend_args)
+
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
 
 load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-person/faster_rcnn_r50_fpn_1x_coco-person_20201216_175929-d022e227.pth'
